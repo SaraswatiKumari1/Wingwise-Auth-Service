83f06f917150592a05f93dad7ee5a5d89c07d22c
SaraswatiKumari1 <kri.saras123@gmail.com>
2025-12-26 19:27:41 +0530

    Impleneted token creation and verification

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const {PORT} = require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
 
+//const UserService = require('./services/user-service');
+
 const app = express();
 
 const prepareAndStartServer = () => {
@@ -15,6 +17,11 @@ const prepareAndStartServer = () => {
 
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
+        // const service  = new UserService();
+        // const token = service.createToken({email: 'saras@admin.com', id: 1});
+        // console.log("Token is ", token);
+        // const response = service.verifyToken(token);
+        // console.log("Response from verify token ", response);
     });
 }
 
