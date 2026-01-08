30510f9c723aa9d76142509f8097e5eb242ef6a9
SaraswatiKumari1 <kri.saras123@gmail.com>
2026-01-08 23:42:32 +0530

    Added user_roles and roles

--- src/index.js
+++ src/index.js
@@ -12,11 +12,20 @@ const prepareAndStartServer = () => {
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
+
+    const db = require('./models/index');
     
     app.use('/api', apiRoutes);
+    //const {User, Role} = require('./models/index');
 
-    app.listen(PORT, () => {
+    app.listen(PORT, async () => {
         console.log(`Server is running on port ${PORT}`);
+        if(process.env.DB_SYNC) {
+            await db.sequelize.sync({alter: true});
+        }
+        // const u1 = await User.findByPk(3);
+        // const r1 = await Role.findByPk(1);
+        // await u1.addRole(r1);
         // const service  = new UserService();
         // const token = service.createToken({email: 'saras@admin.com', id: 1});
         // console.log("Token is ", token);
